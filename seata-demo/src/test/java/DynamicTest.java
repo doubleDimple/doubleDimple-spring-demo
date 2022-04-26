c2b81b3be424e6907a9679492f8dd30076a2a633
= <=>
2022-04-26 14:14:12 +0800

    support more datasource dynamic change

--- seata-demo/src/test/java/DynamicTest.java
+++ seata-demo/src/test/java/DynamicTest.java
@@ -38,4 +38,9 @@ public class DynamicTest {
         product.setName("huawei");
         transactionalService.insertDis(product);
     }
+
+    public static void main(String[] args) {
+        String s = "123";
+        System.out.println(s.replace("'", ""));
+    }
 }
